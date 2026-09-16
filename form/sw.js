0000e974e0304c236305bad2cef686833b35aaab
Adib Asrori <asroriadib@gmail.com>
2026-09-16 14:03:13 +0545

    Use the width that is there: a document measure for the pages, a dashboard width for the hub

--- form/sw.js
+++ form/sw.js
@@ -19,7 +19,7 @@
    old validation, and (16 Sep 2026) a version of store.js that did not sync
    to the register at all. The fix was deployed and had no effect on any
    device until this line changed. */
-const CACHE = "mhpss-np-field-v13";
+const CACHE = "mhpss-np-field-v14";
 
 const PRECACHE = [
   "./",
